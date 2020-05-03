b87a1c3a7b4cc40b02b61c09934abebfaa75296a
Jay Prakash Thakur <jayprakashthakursnr@gmail.com>
2020-05-04 02:29:07 +0530

    architecture created

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -21,17 +20,79 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
-        
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.conv5 = nn.Conv2d(256, 512, 1)
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.pool4 = nn.MaxPool2d(2, 2)
+        self.pool5 = nn.MaxPool2d(2, 2)
         
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # fully-connected layers
+        self.fc1 = nn.Linear(6*6*512, 1024)
+        self.fc2 = nn.Linear(1024, 136)
+        
+#         self.fc1 = nn.Linear(512*6*6, 1024)
+#         self.fc2 = nn.Linear(1024, 512)
+#         self.fc3 = nn.Linear(512, 68*2)
+        
+        # droputs
+        self.dropout = nn.Dropout(p = 0.25)
+        self.drop1 = nn.Dropout(p = 0.1)
+        self.drop2 = nn.Dropout(p = 0.2)
+        self.drop3 = nn.Dropout(p = 0.3)
+        self.drop4 = nn.Dropout(p = 0.4)
+        self.drop5 = nn.Dropout(p = 0.5)
+        self.drop6 = nn.Dropout(p = 0.6)
+
+#         self.drop1 = nn.Dropout(p = 0.1)
+#         self.drop2 = nn.Dropout(p = 0.2)
+#         self.drop3 = nn.Dropout(p = 0.25)
+#         self.drop4 = nn.Dropout(p = 0.25)
+#         self.drop5 = nn.Dropout(p = 0.3)
+#         self.drop6 = nn.Dropout(p = 0.4)
 
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.drop1(x)
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.drop2(x)
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.drop3(x)
+        x = self.pool4(F.relu(self.conv4(x)))
+        x = self.drop4(x)
+        x = self.pool5(F.relu(self.conv5(x)))
+        x = self.drop5(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.drop6(x)
+        x = self.fc2(x)
+
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = self.pool(F.relu(self.conv3(x)))
+#         x = self.pool(F.relu(self.conv4(x)))
+#         x = self.pool(F.relu(self.conv5(x)))
         
+#         # Prep for linear layer / Flatten
+#         x = x.view(x.size(0), -1)
+        
+#         # linear layers with dropout in between
+#         x = F.relu(self.fc1(x))
+#         x = self.dropout(x)
+#         x = F.relu(self.fc2(x))
+#         x = self.dropout(x)
+#         x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
